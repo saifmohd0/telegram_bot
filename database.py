9fe47d4065f8aee45abbb7a97f6ece8331a2e75d
saifmohd0 <sultan0019196@gmail.com>
2025-01-02 14:52:14 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker, declarative_base
 
 
 DATABASE_URI = "mysql+pymysql://root:Saif12345@localhost:3306/telegram_bot_db"
-engine = create_engine(DATABASE_URI, echo=True)  # Enable detailed logging
+engine = create_engine(DATABASE_URI, echo=True) 
 db_session = scoped_session(sessionmaker(bind=engine))
 Base = declarative_base()
 
